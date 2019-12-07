51c82cda305a76a4bfd2676cf0db7a51c094caa3
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2019-12-07 23:47:42 +0100

    add needed headers

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ db.once('open', function () {
 
 app.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
