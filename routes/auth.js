351114d80b55bb1ae2f0412d00d1a0d2495d7b15
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2019-12-07 22:37:38 +0100

    debugging

--- routes/auth.js
+++ routes/auth.js
@@ -84,7 +84,7 @@ router.get('/logout', function (req, res) {
 });
 
 router.get('/isAuth', function (req, res) {
-  console.log(req)
+  console.log(req.user)
   console.log(req.isAuthenticated())
   if (req.isAuthenticated()) {
     res.status(200).send().end();
