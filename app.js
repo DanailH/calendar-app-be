f80325216cd05bdce6dd8d60ad6534e60557a9a0
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2019-12-08 13:26:49 +0100

    asd

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.set('trust proxy', 1) // trust first proxy
 app.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+  // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
